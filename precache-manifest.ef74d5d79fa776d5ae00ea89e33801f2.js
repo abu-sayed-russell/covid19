7adf3de8387009f8d10e487608770a9e91fd5c0e
rsrussell <srsohoj@gmail.com>
2020-04-17 12:37:49 +0600

    Updates

---
+++ precache-manifest.ef74d5d79fa776d5ae00ea89e33801f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc0ad6d3f3e903de488003eca57e864",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "27cfd67466f21477922a",
+    "url": "/covid19/static/css/main.18aa824b.chunk.css"
+  },
+  {
+    "revision": "fcddc1fffd9f8afca096",
+    "url": "/covid19/static/js/2.a108ca0a.chunk.js"
+  },
+  {
+    "revision": "af5111307236c4c69d6adba51f98588a",
+    "url": "/covid19/static/js/2.a108ca0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27cfd67466f21477922a",
+    "url": "/covid19/static/js/main.9461a675.chunk.js"
+  },
+  {
+    "revision": "4d8cf6e9d88aa080a32b",
+    "url": "/covid19/static/js/runtime-main.cec6d2d0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
